03d6987127f31c18e376567f99eecd99045d501b
zaiverse <brenda.garcia007@mymdc.net>
2023-11-26 19:34:40 -0800

    added custom toolbar, added stroke effect to buttons, made text/button adjustments throughout AI, edited weather card info

---
+++ app/src/androidTest/java/com/example/mainactivity/MainActivityTest.kt
@@ -0,0 +1,90 @@
+package com.example.mainactivity
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.mainactivity.activities.MainActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun textView_recommendActAreaTitle_isDisplayed() {
+        onView(withId(R.id.recommend_act_area_title_textView))
+            .check(matches(isDisplayed()))
+            .check(matches(withText(R.string.recommended_activity_area_title)))
+    }
+
+    @Test
+    fun testImageView_isDisplayed() {
+        onView(withId(R.id.recommend_activity_imageView)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testShortDescription_isDisplayed() {
+        onView(withId(R.id.activity_short_desc_textView))
+            .check(matches(withText(R.string.recommended_activity_short_desc_text)))
+    }
+
+    @Test
+    fun testMultilineEditText_isDisplayed() {
+        onView(withId(R.id.editTextTextMultiLine)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testButton_isDisplayedAndClickable() {
+        onView(withId(R.id.generate_rand_act_button)).check(matches(isDisplayed()))
+        onView(withId(R.id.generate_rand_act_button)).perform(click())
+
+        // Add assertions to check the expected behavior after clicking the button.
+        // For example, if clicking the button should display a new text in activity_short_desc_textView:
+        // onView(withId(R.id.activity_short_desc_textView)).check(matches(withText("New text after click")))
+    }
+
+    @Test
+    fun textView_forecastCardTitle_isDisplayed() {
+        onView(withId(R.id.forecast_card_title_textview))
+            .check(matches(isDisplayed()))
+            .check(matches(withText(R.string.weather_card_title)))
+    }
+
+    @Test
+    fun editTextAndButtonInteractionTest() {
+        onView(withId(R.id.zipcode_editTextNumber)).perform(typeText("12345"), closeSoftKeyboard())
+        onView(withId(R.id.zipcode_enter_button)).perform(click())
+        // Assertions or verifications after button click
+    }
+
+    @Test
+    fun recyclerViewDisplayAndInteractionsTest() {
+        onView(withId(R.id.horizontal_card_recyclerview)).check(matches(isDisplayed()))
+        onView(withId(R.id.horizontal_card_recyclerview))
+            .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(5))
+    }
+
+    @Test
+    fun toolbarLogo_isDisplayed() {
+        onView(withId(R.id.toolbarLogo))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun logoutButtonClickTest() {
+        onView(withId(R.id.logoutButton)).perform(click())
+        // Assertions to verify logout behavior, like returning to the login screen
+        // For example, if logout navigates to a login screen:
+        // onView(withId(R.id.login_screen_id)).check(matches(isDisplayed()))
+    }
+
+}
---
+++ app/src/androidTest/java/com/example/mainactivity/RegisterActivityTest.kt
@@ -0,0 +1,96 @@
+package com.example.mainactivity
+
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import android.widget.TextView
+import com.example.mainactivity.activities.RegistrationActivity
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class RegistrationActivityTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(RegistrationActivity::class.java)
+
+    @Test
+    fun registrationActivity_DisplayedElements() {
+        onView(withId(R.id.registration_title_textView)).check(matches(isDisplayed()))
+        onView(withId(R.id.reg_usermame_textinput)).check(matches(isDisplayed()))
+        onView(withId(R.id.reg_password_textinput)).check(matches(isDisplayed()))
+        onView(withId(R.id.reg_retype_pass_textinput)).check(matches(isDisplayed()))
+        onView(withId(R.id.student_faculty_switch)).check(matches(isDisplayed()))
+        onView(withId(R.id.faculty_label)).check(matches(isDisplayed()))
+        onView(withId(R.id.student_label)).check(matches(isDisplayed()))
+        onView(withId(R.id.reg_register_button)).check(matches(isDisplayed()))
+        onView(withId(R.id.existing_user_textView)).check(matches(isDisplayed()))
+        onView(withId(R.id.reg_login_button)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun registrationActivity_InteractionWithSwitch() {
+        // Initial state should be 'Student'
+        onView(withId(R.id.student_label)).check(matches(withTextColor(R.color.PA_Creek)))
+        onView(withId(R.id.faculty_label)).check(matches(withTextColor(R.color.White_Out)))
+
+        // Click the switch to change to 'Faculty'
+        onView(withId(R.id.student_faculty_switch)).perform(click())
+
+        // Check colors are updated
+        onView(withId(R.id.faculty_label)).check(matches(withTextColor(R.color.PA_Creek)))
+        onView(withId(R.id.student_label)).check(matches(withTextColor(R.color.White_Out)))
+
+        // Click again to change back to 'Student'
+        onView(withId(R.id.student_faculty_switch)).perform(click())
+
+        // Check colors are reverted
+        onView(withId(R.id.student_label)).check(matches(withTextColor(R.color.PA_Creek)))
+        onView(withId(R.id.faculty_label)).check(matches(withTextColor(R.color.White_Out)))
+    }
+
+    @Test
+    fun registrationActivity_CompleteFormAndRegister() {
+        onView(withId(R.id.reg_usermame_textinput)).perform(typeText("NewUser"), closeSoftKeyboard())
+        onView(withId(R.id.reg_password_textinput)).perform(typeText("Password123"), closeSoftKeyboard())
+        onView(withId(R.id.reg_retype_pass_textinput)).perform(typeText("Password123"), closeSoftKeyboard())
+        onView(withId(R.id.reg_register_button)).perform(click())
+
+        // Verify that the registration was successful or that the next expected behavior occurs
+        // For example, if a success message is displayed:
+        // onView(withText("Registration successful")).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun registrationActivity_NavigateBackToLogin() {
+        onView(withId(R.id.reg_login_button)).perform(click())
+
+        // Verify that the app navigates back to the LoginActivity
+        // onView(withId(R.id.login_activity_view_id)).check(matches(isDisplayed()))
+    }
+
+    // Custom matcher for checking text color
+    fun withTextColor(expectedColor: Int): Matcher<View> {
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("with text color: ")
+                description.appendValue(expectedColor)
+            }
+
+            override fun matchesSafely(view: View): Boolean {
+                val textView = view as TextView
+                val colorId = ContextCompat.getColor(view.context, expectedColor)
+                return textView.currentTextColor == colorId
+            }
+        }
+    }
+}
