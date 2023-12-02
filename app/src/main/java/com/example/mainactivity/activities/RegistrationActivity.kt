b388408d089034fd8cf03e5fabc1a9f92b836d84
Kevin Fox <93135643+kpf5297@users.noreply.github.com>
2023-12-01 21:39:12 -0500

    added binding to registration activity and comments (#54)

--- app/src/main/java/com/example/mainactivity/activities/RegistrationActivity.kt
+++ app/src/main/java/com/example/mainactivity/activities/RegistrationActivity.kt
@@ -6,12 +6,24 @@ import android.widget.TextView
 import androidx.appcompat.widget.SwitchCompat
 import androidx.core.content.ContextCompat
 import com.example.mainactivity.R
+import com.example.mainactivity.databinding.ActivityRegistrationBinding
 
 class RegistrationActivity : AppCompatActivity() {
+
+    // Added binding variable KF 12/1/2023
+    private lateinit var binding: ActivityRegistrationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration)
 
+        // Added binding variable for persistent data KF 12/1/2023
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Removed old view binding code KF 12/1/2023
+        // setContentView(R.layout.activity_registration)
+
+        // Set the switch state change listener
         val switchCompat: SwitchCompat = findViewById(R.id.student_faculty_switch)
         val facultyLabel: TextView = findViewById(R.id.faculty_label)
         val studentLabel: TextView = findViewById(R.id.student_label)
@@ -26,6 +38,7 @@ class RegistrationActivity : AppCompatActivity() {
         }
     }
 
+    // Update the label colors based on the switch state
     private fun updateLabelColors(facultyLabel: TextView, studentLabel: TextView, isChecked: Boolean) {
         if (isChecked) {
             facultyLabel.setTextColor(ContextCompat.getColor(this, R.color.White_Out)) // Inactive color
