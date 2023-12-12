8fa8b6e01173627c588513c8a3c40855faebfb80
EK <71151325+emilyyankan@users.noreply.github.com>
2023-12-12 13:13:09 -0800

    Finalizing WeatherAPI integration in Faculty (#68)

--- app/src/main/java/com/example/mainactivity/activities/FacultyActivity.kt
+++ app/src/main/java/com/example/mainactivity/activities/FacultyActivity.kt
@@ -9,7 +9,6 @@ import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.SnapHelper
 import com.example.mainactivity.BuildConfig
 import com.example.mainactivity.R
 import com.example.mainactivity.adapters.WeatherFacultyAdapter
@@ -20,18 +19,16 @@ import com.example.mainactivity.model.WeatherFacultyItem
 import com.example.mainactivity.model.network.RetrofitInstance
 import com.example.mainactivity.repository.WeatherRepository
 import com.example.mainactivity.utils.WeatherDataConverter
-import com.google.android.material.textfield.TextInputEditText
 import retrofit2.Retrofit
 
 class FacultyActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFacultyBinding
     private lateinit var facultyController: FacultyWeatherController
     private lateinit var weatherFacultyAdapter: WeatherFacultyAdapter
-    private lateinit var weatherService: WeatherService
     private lateinit var dataConverter: WeatherDataConverter
+    private lateinit var weatherService: WeatherService
     private lateinit var retrofit: Retrofit
-    private lateinit var snapHelper: SnapHelper
-
+    private lateinit var snapHelper: PagerSnapHelper
     private val defaultZipCode = "16802"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,12 +39,18 @@ class FacultyActivity : AppCompatActivity() {
         setupFacultyToolbar()
         setupFacultyListeners()
         initializeFacultyWeatherController()
-        loadDefaultFacultyWeatherData()
 
         dataConverter = WeatherDataConverter
         snapHelper = PagerSnapHelper()
+        loadDefaultFacultyWeatherData()
     }
 
+    private fun initializeFacultyWeatherController() {
+        retrofit = RetrofitInstance.retrofit
+        weatherService = retrofit.create(WeatherService::class.java)
+        val weatherRepository = WeatherRepository(weatherService)
+        facultyController = FacultyWeatherController(weatherRepository, facultyWeatherCallback)
+    }
 
     private fun setupFacultyToolbar() {
         val toolbar: Toolbar = binding.toolbarFaculty
@@ -56,77 +59,60 @@ class FacultyActivity : AppCompatActivity() {
     }
 
     private fun setupFacultyListeners() {
-        setupZipcodeFaultyListener()
-        setupLogoutFacultyListener()
-    }
-
-    private fun setupTabLayout(itemCount: Int) {
-        val tabLayout = binding.tabDotsFaculty
-        tabLayout.removeAllTabs()
-        for (i in 0 until itemCount)
-            tabLayout.addTab(tabLayout.newTab())
-    }
-
-    private fun setupZipcodeFaultyListener() {
         binding.zipcodeFacultyEnterButton.setOnClickListener {
             handleZipcodeEntry()
         }
-    }
-
-    private fun setupLogoutFacultyListener() {
         binding.logoutButtonFaculty.setOnClickListener {
             showLogoutMsg()
         }
     }
 
-    private fun initializeFacultyWeatherController() {
-        retrofit = RetrofitInstance.retrofit
-        weatherService = retrofit.create(WeatherService::class.java)
-        val weatherRepository = WeatherRepository(weatherService)
-        facultyController = FacultyWeatherController(weatherRepository, createFacultyWeatherCallback())
-    }
-
     private fun handleZipcodeEntry() {
-        val zipcodeFacultyEditText: EditText = findViewById<TextInputEditText>(R.id.zipcode_faculty)
+        val zipcodeFacultyEditText: EditText = findViewById(R.id.zipcode_faculty)
         val enteredPostalCode = zipcodeFacultyEditText.text.toString()
         facultyController.fetchFacultyWeatherForecast(enteredPostalCode, BuildConfig.API_KEY, "imperial")
     }
 
-    private fun createFacultyWeatherCallback(): FacultyWeatherController.FacultyWeatherCallback {
-        return object : FacultyWeatherController.FacultyWeatherCallback {
-            override fun onSuccess(weatherFacultyData: List<WeatherFacultyItem>) {
-                runOnUiThread {
-                    if (weatherFacultyData != null) {
-                        // Process and round temperatures here
-                        val processedData =
-                            dataConverter.processAndRoundWeatherFacultyData(weatherFacultyData)
-
-                        val dailyData =
-                            dataConverter.aggregateWeatherFacultyDataByDay(processedData)
-                        setupRecyclerView(dailyData) // Initialize RecyclerView with API data
-                        setupTabLayout(dailyData.size)
-                    }
-                }
-            }
-
-            override fun onError(error: String) {
-                runOnUiThread { showErrorDialog(error) }
-            }
-        }
+    private fun loadDefaultFacultyWeatherData() {
+        facultyController.fetchFacultyWeatherForecast(defaultZipCode, BuildConfig.API_KEY, "imperial")
     }
 
+    private val facultyWeatherCallback = object : FacultyWeatherController.FacultyWeatherCallback {
+        override fun onSuccess(weatherFacultyData: List<WeatherFacultyItem>) {
+            runOnUiThread {
+                if (weatherFacultyData.isNotEmpty()) {
+                    val processedData = dataConverter.processAndRoundWeatherFacultyData(weatherFacultyData)
+
+                    val dailyData = dataConverter.aggregateWeatherFacultyDataByDay(processedData)
+
+                    setupRecyclerView(dailyData)
+                    setupTabLayout(weatherFacultyData.size)
+                } else {
+                    showErrorDialog("No data available")
+                }
+            }
+        }
+
+        override fun onError(error: String) {
+            runOnUiThread { showErrorDialog(error) }
+        }
+    }
 
     private fun setupRecyclerView(weatherFacultyItems: List<WeatherFacultyItem>) {
         weatherFacultyAdapter = WeatherFacultyAdapter(weatherFacultyItems)
         binding.horizontalFacultyCardRecyclerview.apply {
             layoutManager = LinearLayoutManager(this@FacultyActivity, LinearLayoutManager.HORIZONTAL, false)
-            weatherFacultyAdapter = weatherFacultyAdapter
-
-            // Attach SnapHelper
+            adapter = weatherFacultyAdapter
             snapHelper.attachToRecyclerView(this)
         }
+    }
 
-        binding.horizontalFacultyCardRecyclerview.addOnScrollListener(createOnScrollListener())
+    private fun setupTabLayout(itemCount: Int) {
+        val tabLayout = binding.tabDotsFaculty
+        tabLayout.removeAllTabs()
+        for (i in 0 until itemCount) {
+            tabLayout.addTab(tabLayout.newTab())
+        }
     }
 
     private fun createOnScrollListener() = object : RecyclerView.OnScrollListener() {
@@ -138,13 +124,6 @@ class FacultyActivity : AppCompatActivity() {
                 binding.tabDotsFaculty.selectTab(binding.tabDotsFaculty.getTabAt(position))
             }
         }
-    }
-
-    private fun loadDefaultFacultyWeatherData() {
-        facultyController.fetchFacultyWeatherForecast(
-            defaultZipCode,
-            BuildConfig.API_KEY,
-            "imperial")
     }
 
     private fun showLogoutMsg() {
